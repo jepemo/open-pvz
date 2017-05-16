3ac5ac97df5ea7d6d08daad4dc19f95d8bb83548
jepemo <Jeremias.Perez@desfici.com>
2017-05-16 20:24:35 +0200

    initializing structures

--- src/opvz_level.c
+++ src/opvz_level.c
@@ -1,8 +1,40 @@
 #include <stdlib.h>
+#include <string.h>
 #include "opvz_level.h"
 
-Level *
-level_new (void)
+
+LevelConfig *
+level_config_new(size_t rows, size_t cols, short depth)
 {
-  return NULL;
+  LevelConfig *config = malloc(sizeof(LevelConfig));
+  config->n_rows = rows;
+  config->n_cols = cols;
+  config->init_depth = depth;
+
+  return config;
+}
+
+Level *
+level_new (LevelConfig *config)
+{
+  Level *level = malloc(sizeof(Level));
+
+  level->config = config;
+
+  short dimension = (level->config->n_rows * level->config->n_cols);
+
+  level->depth = malloc(dimension * sizeof(short));
+  memset(level->depth, 0, dimension);
+
+  return level;
+}
+
+/**
+ * Create a map of 5x9, with depth 0
+ */
+Level *
+level_new_default(void)
+{
+  LevelConfig * config = level_config_new(5, 9, 0);
+  return level_new(config);
 }
--- src/opvz_level.h
+++ src/opvz_level.h
@@ -7,11 +7,26 @@
 #define __OPEN_PVZ_H__
 
 typedef struct {
-  int n_rows;
-  int n_cols;
+  /* Number of map rows */
+  size_t n_rows;
+  /* Number of map cols */
+  size_t n_cols;
+  /* Default value to init depth */
+  short init_depth;
+} LevelConfig;
+
+typedef struct {
+  /* Level configuration*/
+  LevelConfig *config;
+  /* Surface profundity. Default is 0 */
   short **depth;
+  /* Game objects in the level */
+  short ***level_objects;
 } Level;
 
-Level * level_new (void);
+LevelConfig * level_config_new(size_t rows, size_t cols, short depth);
+
+Level * level_new        (LevelConfig *config);
+Level * level_new_default(void);
 
 #endif /* __OPEN_PVZ_H__ */
